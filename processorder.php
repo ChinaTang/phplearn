06bdb7b254b600f03d30b53a519163c797e5a3d2
tangdi <tangdi0802@foxmail.com>
2017-07-13 21:07:58 +0800

    first commit

---
+++ processorder.php
@@ -0,0 +1,16 @@
+
+<?php
+echo '1111'.'<br>';
+    $tireqty = $_REQUEST['tireqty'];
+//    echo "<p>Order processed at";
+//    echo date('H:i, jS SSF Y') ;
+//    echo "</P>";
+   echo $tireqty."<br/>";
+   $totalqty = 0;
+   $totalmount = $totalqty;
+   define('TIREPRICE', 100);
+   define('OILPRICE', 10);
+   define('SPARKPRICE', 4);
+   echo TIREPRICE;
+   phpinfo();
+?>
